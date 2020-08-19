5f67422b4f71c92f388bcb2afe561f7e0517b90b
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2020-08-19 14:35:07 +0600

    Update nayeem.js

--- nayeem.js
+++ nayeem.js
@@ -1,2 +1,35 @@
-var vapapitha=new Vapa();
-console.log(vapapitha);
+//var vapapitha=new Vapa();
+//console.log(vapapitha);
+//My creat (This C language Code)!!!
+//What is the output????????    
+
+#include<stdio.h>
+int main()
+{
+  while(1){
+
+    static int i,j,k;
+    i=0;j=2;k=2;
+    char *ara="bmlxoyoqdsy";
+    char *arax="utfsyurjclbk";
+    char *aral="01ysoowqu";
+
+   for(;i<11;){
+      printf("%c",*(ara+i));
+      i=i+2;
+   }
+   printf(" ");
+    for(;j<12;){
+      printf("%c",*(arax+j));
+      j=j+3;
+   }
+   printf(" ");
+    for(;k<9;){
+      printf("%c",*(aral+k));
+      k=k+3;
+   }
+   printf("\n\n");
+
+  }
+   return 0;
+}
