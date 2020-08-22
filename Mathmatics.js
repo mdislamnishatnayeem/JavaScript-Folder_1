23e716440116a9fbd0dfefe901a9c8592edefdb1
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2020-08-22 19:25:26 +0600

    Mathmatics in js code

---
+++ Mathmatics.js
@@ -0,0 +1,32 @@
+//mathmatics in js 
+
+document.write(23*56+78);
+document.write("\n");//make a space
+document.write("<br>");// make a line break
+document.write(23*(23+23));
+
+
+
+
+//more math matical js code
+
+//  var number=6;
+ //  document.write(number*(4+8));
+
+ var number=-3;
+  document.write(Math.abs(number));
+  document.write("<br>");
+  document.write(Math.max(number,4));
+  document.write ("<br>");
+  document.write(Math.min(number,4));
+  var m= 8.678;
+  document.write ("<br>");
+  document.write (Math.round(m));
+  document. write ("<br>");
+  document. write (Math.pow(2,3));
+  document. write ("<br>");
+  document. write (Math.sqrt(81));
+  document. write ("<br>");
+  var i=2;
+  document. write (Math.round(Math.random()*10));
+
