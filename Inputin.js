830c351522dea2e9777f5d5d8e3b741009928ab5
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2020-08-22 14:03:46 +0600

    Get in put in javascript

---
+++ Inputin.js
@@ -0,0 +1,8 @@
+//input in javascript
+
+//var name = window.prompt("Enter your name: ");
+//alert("Your name is " + name);
+var num=window.prompt("Inter a number: ");
+var num2=window.prompt(" Inter a another number: ");
+var sum=num+num2;
+alert(" The sum is "+sum);
