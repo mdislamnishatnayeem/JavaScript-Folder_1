1e6cde538a6edf0ffbced9a799e9ff9259c8ed31
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2020-08-23 18:07:44 +0600

    Js code count word

---
+++ Count word.js
@@ -0,0 +1,23 @@
+//will count word frome a sentence
+//problem-if i give a space at the very fast
+//of the sentence then it count one more
+//here is the problem
+
+var andolon="Rokto   jokhon    diyeci tokhin aro debo.  kintu   ";
+var count=0;
+var n=andolon.length;
+  
+for(var i=0;i<andolon.length;i++){
+ if(andolon[i]==" "){
+    count++;
+  if(andolon[i+1]==" "){
+     count--;
+     }
+  }
+}
+    
+if(andolon[n-1]==" "){
+  count--;
+ }
+count++;
+console.log(count);
