40ac0e5c7929f230ce20794ea147638323e55992
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2020-08-24 13:09:20 +0600

    Word count frome sentence

--- Count word.js
+++ Count word.js
@@ -21,3 +21,35 @@ if(andolon[n-1]==" "){
  }
 count++;
 console.log(count);
+
+
+
+//now i will make it function. The code bellow
+
+
+function gonona(andolon){
+
+var count=0;
+var n=andolon.length;
+  
+for(var i=0;i<andolon.length;i++){
+ if(andolon[i]==" "){
+    count++;
+  if(andolon[i+1]==" "){
+     count--;
+     }
+  }
+}
+  
+ 
+    
+if(andolon[n-1]==" "){
+  count--;
+ }
+count++;
+   return count;
+}
+
+
+var lol=gonona("nayeem islam nishat");
+console.log(lol);
