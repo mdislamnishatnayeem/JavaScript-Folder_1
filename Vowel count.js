3b39ec02aa9490afe80a1417dce689f08110ae86
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2020-08-24 13:54:22 +0600

    Count vowel frome sentence

--- Vowel count.js
+++ Vowel count.js
@@ -12,3 +12,25 @@ for(i=0;i<all.length;i++){
     }
   }
 console.log(count);
+
+
+
+//now i will use here function, the code bellow
+
+function vowelc(n){
+
+var all="aeiouAEIOU";
+var count=0;
+for(i=0;i<all.length;i++){
+  for (var j=0;j<n.length;j++){
+    if(all[i]==n[j]){
+      count++;
+      }
+    }
+  }
+  return count;
+}
+
+var m=vowelc("nayeema");
+console.log(m);
+ 
