2314a3cd6ad7ce6d78ce3b793d891b6765a36927
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2020-08-27 00:16:01 +0600

    Update 2.3 in choddojusti.js

--- 2.3 in choddojusti.js
+++ 2.3 in choddojusti.js
@@ -20,3 +20,35 @@ for( var i = 0; i< names.length ;i++){
  return final;
 }
 console.log(getOddAmount([34,33,34,76,67,76,4,4,4]));
+
+
+
+
+/*আমার এক বড় ভাইয়া আমাকে এই আরেক্টি সহজ পধধতি 
+অবল্মবন করে code টি লিখটে help করেছিল,যদিও আমার 
+অ জথেস্ত অবদান  রয়েছে।।।।।।। নিচে কডটি*/
+
+
+var arr = [23,24,25,25,23,24,23,23,23,65];
+var arax = [];
+
+for(var i=0; i<arr.length; i++)
+{
+	var one = arr[i];
+	var count = 0;
+	
+	for(var j=0; j<arr.length; j++)
+	{
+		if(arr[i]==arr[j])
+			count++;
+	}
+	
+	
+	if(count%2==1)
+		if(!arax.includes(one))
+			arax.push(one);
+	
+	
+}
+
+console.log(arax);
